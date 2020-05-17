754260ba020ef7c69d4b2e5010e0ac7f4f06eab5
valrus <imccowan@gmail.com>
2020-05-16 22:02:29 -0700

    add album queueing

--- src/actions/play.py
+++ src/actions/play.py
@@ -4,12 +4,16 @@ from __future__ import print_function
 import json
 import os
 
-from util import open_mpd_client
+from util import open_mpd_client, alfred_json
 
 
 def main():
     with open_mpd_client() as client:
-        client.clear()
+        # state is play, pause or stop
+        state = client.status()['state']
+        if (state == 'pause' or state == 'stop') and not os.environ.get('ALFRED_MPD_QUEUE'):
+            client.clear()
+
         variables = {}
         album = os.environ['ALFRED_MPD_ALBUM']
         query_args = ['album', album]
@@ -27,13 +31,10 @@ def main():
         client.findadd(*query_args)
         if os.environ.get('ALFRED_MPD_SHUFFLE'):
             client.shuffle()
-        print(json.dumps({
-            "alfredworkflow": {
-                "arg": track or album,
-                "variables": variables
-            }
-        }))
-        client.play()
+        print(alfred_json(track or album, variables=variables))
+
+        if not os.environ.get('ALFRED_MPD_QUEUE'):
+            client.play()
 
 
 if __name__ == '__main__':
